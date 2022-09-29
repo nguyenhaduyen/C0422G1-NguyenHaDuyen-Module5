42fa9a3aa797424c579fac76ce9446f4047621b6
nguyenhaduyen <nguyenhaduyen23@gmail.com>
2022-09-29 09:09:13 +0700

    bt

--- case-study-furama/src/app/component/service/service-list/service-list.component.ts
+++ case-study-furama/src/app/component/service/service-list/service-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Facility} from './facility';
 
 @Component({
   selector: 'app-service-list',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-list.component.css']
 })
 export class ServiceListComponent implements OnInit {
-
+facility: Facility[]=[{facilityName: "Phòng hướng vườn",facilityArea: 2, facilityCost: 3.5,
+numberOfPeople:12, rentType: "tháng", standardFacility: "Vip", descriptionOther: null, poolArea: 12,
+numberOfFloor: 3, serviceFree: null,img: "https://furamavietnam.com/wp-content/uploads/2018/03/%5E19BFC67D7D58B6D3C155DB9CA96927213F31F8C8E7F909F3E2%5Epimgpsh_fullsize_distr-1047x563.jpg"},
+  {facilityName: "Phòng hướng vườn",facilityArea: 2, facilityCost: 3.5,
+    numberOfPeople:12, rentType: "tháng", standardFacility: "Vip", descriptionOther: null, poolArea: 12,
+    numberOfFloor: 3, serviceFree: null,img: "https://furamavietnam.com/wp-content/uploads/2018/03/%5E19BFC67D7D58B6D3C155DB9CA96927213F31F8C8E7F909F3E2%5Epimgpsh_fullsize_distr-1047x563.jpg"},{facilityName: "Phòng hướng vườn",facilityArea: 2, facilityCost: 3.5,
+    numberOfPeople:12, rentType: "tháng", standardFacility: "Vip", descriptionOther: null, poolArea: 12,
+    numberOfFloor: 3, serviceFree: null,img: "https://furamavietnam.com/wp-content/uploads/2018/03/%5E19BFC67D7D58B6D3C155DB9CA96927213F31F8C8E7F909F3E2%5Epimgpsh_fullsize_distr-1047x563.jpg"},{facilityName: "Phòng hướng vườn",facilityArea: 2, facilityCost: 3.5,
+    numberOfPeople:12, rentType: "tháng", standardFacility: "Vip", descriptionOther: null, poolArea: 12,
+    numberOfFloor: 3, serviceFree: null,img: "https://furamavietnam.com/wp-content/uploads/2018/03/%5E19BFC67D7D58B6D3C155DB9CA96927213F31F8C8E7F909F3E2%5Epimgpsh_fullsize_distr-1047x563.jpg"},{facilityName: "Phòng hướng vườn",facilityArea: 2, facilityCost: 3.5,
+    numberOfPeople:12, rentType: "tháng", standardFacility: "Vip", descriptionOther: null, poolArea: 12,
+    numberOfFloor: 3, serviceFree: null,img: "https://furamavietnam.com/wp-content/uploads/2018/03/%5E19BFC67D7D58B6D3C155DB9CA96927213F31F8C8E7F909F3E2%5Epimgpsh_fullsize_distr-1047x563.jpg"},{facilityName: "Phòng hướng vườn",facilityArea: 2, facilityCost: 3.5,
+    numberOfPeople:12, rentType: "tháng", standardFacility: "Vip", descriptionOther: null, poolArea: 12,
+    numberOfFloor: 3, serviceFree: null,img: "https://furamavietnam.com/wp-content/uploads/2018/03/%5E19BFC67D7D58B6D3C155DB9CA96927213F31F8C8E7F909F3E2%5Epimgpsh_fullsize_distr-1047x563.jpg"}]
   constructor() { }
 
   ngOnInit() {
